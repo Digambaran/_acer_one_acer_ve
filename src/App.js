77a0b3d198340207d17865dd60fce2c212a22d38
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-21 20:06:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import acer_ve from './acer_ve'
+export default function App() {
+  return (
+    <div>
+      <acer_ve/>
+    </div>
+  )
+}
---
+++ src/acer_ve.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const acer_ve = () => {
+  return (
+   <p>Hello from acer_ve </p>
+  )
+}
+export default acer_ve
